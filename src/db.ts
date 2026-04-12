a482a2f540b35d1a13e5710dffcd0e245056db7c
wintercoder <winter_coder404@proton.me>
2026-04-12 11:47:21 -0400

    added procfile and updated db client to support railways database_url

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,15 @@
 import postgres from "postgres";
 
-const sql = postgres({
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  max: 10,
-  idle_timeout: 30,
-});
+const sql = process.env.DATABASE_URL
+  ? postgres(process.env.DATABASE_URL, { max: 10, idle_timeout: 30 })
+  : postgres({
+      host: process.env.DB_HOST!,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME!,
+      user: process.env.DB_USER!,
+      password: process.env.DB_PASSWORD!,
+      max: 10,
+      idle_timeout: 30,
+    });
 
 export default sql;
